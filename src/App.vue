852fab2842cf87997aacff64e718fa6f2255c5af
Arda Kaya <arrdakaya@hotmail.com>
2025-11-05 20:01:39 +0300

    release date

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <div class="release-date-banner">
+      <div class="banner-content">
+        <img :src="currentLeftImage" alt="Character" class="banner-icon-img" />
+        <span class="banner-text"><strong>20 • NOVEMBER • 2025</strong></span>
+        <img :src="currentRightImage" alt="Character" class="banner-icon-img" />
+      </div>
+    </div>
     <ParticleSystem />
     <Navigation />
     <router-view />
@@ -13,6 +20,12 @@ import ParticleSystem from './components/ParticleSystem.vue'
 import Navigation from './components/Navigation.vue'
 import Footer from './components/Footer.vue'
 import ApplicationModal from './components/ApplicationModal.vue'
+import HunterCircle from './assets/images/HunterCircle.png'
+import JenniferCircle from './assets/images/JenniferCircle.png'
+import JakeCircle from './assets/images/JakeCircle.png'
+import JasonCircle from './assets/images/JasonCircle.png'
+import JaneCircle from './assets/images/JaneCircle.png'
+
 export default {
   name: 'App',
   components: {
@@ -20,6 +33,41 @@ export default {
     Navigation,
     Footer,
     ApplicationModal,
+  },
+  data() {
+    return {
+      images: [
+        HunterCircle,
+        JenniferCircle,
+        JakeCircle,
+        JasonCircle,
+        JaneCircle,
+      ],
+      currentLeftIndex: 0,
+      currentRightIndex: 1,
+    }
+  },
+  computed: {
+    currentLeftImage() {
+      return this.images[this.currentLeftIndex]
+    },
+    currentRightImage() {
+      return this.images[this.currentRightIndex]
+    },
+  },
+  mounted() {
+    // Sol taraftaki görseli her 3 saniyede bir değiştir
+    setInterval(() => {
+      this.currentLeftIndex = (this.currentLeftIndex + 1) % this.images.length
+    }, 3000)
+
+    // Sağ taraftaki görseli her 3 saniyede bir değiştir (1.5 saniye gecikmeyle)
+    setTimeout(() => {
+      setInterval(() => {
+        this.currentRightIndex =
+          (this.currentRightIndex + 1) % this.images.length
+      }, 3000)
+    }, 1500)
   },
 }
 </script>
